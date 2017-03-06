1b88eacaf09daef88815c9d30e60e2973b34a9b7
phanluongha <chonthienduonghoangvang2009@yahoo.com>
2017-03-06 22:52:36 +0700

    22:52/6-3/2017

---
+++ app/src/main/java/com/example/phanluongha/myfirstapplication/impl/ExhibitionFavoriteClickListener.java
@@ -0,0 +1,10 @@
+package com.example.phanluongha.myfirstapplication.impl;
+
+/**
+ * Created by PhanLuongHa on 2/28/2017.
+ */
+
+public interface ExhibitionFavoriteClickListener {
+
+    public void keyClickedIndex(int position);
+}
